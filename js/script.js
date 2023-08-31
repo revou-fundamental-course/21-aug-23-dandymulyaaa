e8b80de25950477b2490db0abed0ab84151c1877
dandymulyaaa <dandymulya30@gmail.com>
2023-08-31 14:11:56 +0700

    validasi if message nya kosong

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-const buttonKirim = document.querySelector('.kirim');
+const buttonKirim = document.querySelector('.kirim')
 buttonKirim.addEventListener('click', () => {
-  const namaHtml = document.querySelector('.label2');
+  const namaHtml = document.querySelector('.label2')
   const tanggalHtml = document.querySelector('.label3')
   const pesanHtml = document.querySelector('.label4')
-  let maleRadio = document.getElementById("maleRadio");
-  let femaleRadio = document.getElementById("femaleRadio");
-  document.querySelector('.outputNama').innerHTML = `<span>Nama : ${namaHtml.value}</span>`;
-  document.querySelector('.outputtgllahir').innerHTML = `<span>Tanggal Lahir : ${tanggalHtml.value}</span>`;
-  document.querySelector('.outputpesan').innerHTML = `<span>Pesan : ${pesanHtml.value}</span>`;
+  let maleRadio = document.getElementById('maleRadio')
+  let femaleRadio = document.getElementById('femaleRadio')
+  if (namaHtml.value === '' || tanggalHtml.value === '' || pesanHtml.value === '') {
+    alert('HARAP DIISI')
+  } else {
+    document.querySelector('.outputNama').innerHTML = `<span>Nama : ${namaHtml.value}</span>`
+    document.querySelector('.outputtgllahir').innerHTML = `<span>Tanggal Lahir : ${tanggalHtml.value}</span>`
+    document.querySelector('.outputpesan').innerHTML = `<span>Pesan : ${pesanHtml.value}</span>`
+  }
   if (maleRadio.checked) {
     document.querySelector('.outputjeniskelamin').innerHTML = '<span>Jenis Kelamin : Laki-Laki</span>'
   } else if (femaleRadio.checked) {
     document.querySelector('.outputjeniskelamin').innerHTML = '<span>Jenis Kelamin : Perempuan</span>'
   }
-  let dateNowElement = document.querySelector('.outputDateNow');
+  let dateNowElement = document.querySelector('.outputDateNow')
   function updateDateNow() {
-    let now = new Date();
-    let options = { 
+    let now = new Date()
+    let options = {
       weekday: 'long',
       year: 'numeric',
       month: 'long',
@@ -24,30 +28,25 @@ buttonKirim.addEventListener('click', () => {
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric',
-      timeZoneName: 'short'
-    };
-    let formattedDate = now.toLocaleString('en-US', options);
-    dateNowElement.innerHTML = `<span>Waktu Saat Ini:  ${formattedDate}</span>`;
+      timeZoneName: 'short',
+    }
+    let formattedDate = now.toLocaleString('en-US', options)
+    dateNowElement.innerHTML = `<span>Waktu Saat Ini:  ${formattedDate}</span>`
 
-    document.querySelector('#titlename').innerHTML = namaHtml.value;
+    document.querySelector('#titlename').innerHTML = namaHtml.value
   }
-  updateDateNow();
+  updateDateNow()
 })
 
-const links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      if (target) {
-        target.scrollIntoView({
-          behavior: "smooth"
-        });
-      }
-    });
-  });
-
-
-
-
-
+const links = document.querySelectorAll('a[href^="#"]')
+links.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    const target = document.querySelector(this.getAttribute('href'))
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+      })
+    }
+  })
+})
